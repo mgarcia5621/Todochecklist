1094860edea03258bd4abe12784a33828e4d127b
Mateo <matthew27garcia@gmail.com>
2022-05-05 23:27:31 -0500

    added the js to being able to filter out the completed things

--- scripts.js
+++ scripts.js
@@ -66,5 +66,17 @@ function deleteCheck(e) {
 
 function filterTodo(e) {
     const todos = todoList.childNodes;
-    console.log(todos);
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+                case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+        }
+    });
 }
